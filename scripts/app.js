c43bd6939bc6329cfdde5eccbf76ec9c8e0670e3
Brian Meraz <briangerimeraz@Meraz-MBP.home>
2016-04-13 11:19:07 -0700

    updates

--- scripts/app.js
+++ scripts/app.js
@@ -158,7 +158,7 @@
 		var finalOffset = 0;
 	}
 	
-	var url = 'https://localhost:8080/data?offset='+finalOffset;
+	var url = 'http://localhost:8080/data?offset='+finalOffset;
     
         
     /*
